c4be2be07a1345326234dd7705a370b3317bafc7
Phil Murphy <phillipdeanmurphy@gmail.com>
2021-09-02 21:53:02 -0700

    got the email in the schema

--- models/users.js
+++ models/users.js
@@ -3,18 +3,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  homeAddress: { type: String, required: true },
+  email: { type: String, },
+  homeLat: { type: String, required: true },
+  homeLon: { type: String, required: true },
   curEmployer: { type: String},
-  employerLoc: { type: String, required: true },
-  curSalary: { type: Number, required: true },
-  curRemote: { type: Boolean, required: true},
-  commuteDist: { type: Number, required: true},
-  milesPerGal: { type: Number, required: true},
-  newSalary: { type: Number },
-  newEmployer: { type: String },
-  newRemote: { type: Boolean },
-  newLocation: { type: String }
+  employerLoc: { type: String, },
+  curSalary: { type: Number, },
+  curRemote: { type: Boolean,},
+  commuteDist: { type: Number,},
+  milesPerGal: { type: Number,},
+  newJob: [{
+    newSalary: { type: Number },
+    newEmployer: { type: String },
+    newRemote: { type: Boolean },
+    newLocation: { type: String }
+  }]
 });
 
 const BookModel = mongoose.model('user', userSchema);
