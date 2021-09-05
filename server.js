d11bb98515e07cc707fc0cd82459e220fddea162
QPThree <youngqp3@gmail.com>
2021-09-05 08:39:36 -0700

    data is now sent from database to front end

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.use(cors());
 
 app.use(express.json());
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/remote-rate', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(async () => {
+  console.log('Connected to the database');
+});
+
+require('dotenv').config();
+
+const UserModel = require('./models/users.js');
+const PORT = process.env.PORT;
+
+
 const jwt = require('jsonwebtoken');
 
 // all of this came from jsonwebtoken docs
@@ -53,68 +67,16 @@ app.get('/landing', (request, response) => {
     response.status(500).send('Auth0 Error')
   }
 });
-
 app.get('/profile', (request, response) => {
   try {
-    UserModel.find({}, (error, userData) => {
-      response.status(200).send(userData);
-    })
+      UserModel.find({}, (error, userData) => {
+        console.log(userData);
+        response.status(200).send(userData);
+      })
   } catch (error) {
     response.status(500).send(error);
   }
 })
-
-
-// app.put('/newoffer/:id', async(request, response) => {
-//   try {
-//     console.log('this is a new offer', request.body);
-//     const id = request.params.id;
-
-//     let updatedUser = await UserModel.findByIdAndUpdate(
-//       id,
-//       { newJob: request.body },
-//       { new: true, overwrite: true }
-//     );
-//     response.status(200).send(updatedUser);
-//   } catch (err) {
-//     response.status(500).send('Error in server when adding offer');
-//   }
-// });
-
-
-app.put('/newoffer/:id', async(request, response) => {
-  try {
-    console.log('this is a new offer', request.body);
-    const id = request.params.id;
-
-    let retrievedUser = await UserModel.find(
-      {_id: id},
-      
-    );
-    console.log('before retrievedUser ',retrievedUser.newJob);
-    retrievedUser.newJob = request.body
-    console.log('after retrievedUser ',retrievedUser.newJob);
-
-    response.status(200).send(retrievedUser);
-  } catch (err) {
-    response.status(500).send('Error in server when adding offer');
-  }
-});
-
-// app.post('/profile', async (request, response) => {
-//   let { newSalary, newEmployer, newRemote, newLocation, workLat, workLon } = request.body;
-//   try {
-//     let updatedUser = await UserModel.findByIdAndUpdate(
-//       id,
-//       { newSalary, newEmployer, newRemote, newLocation, workLat, workLon },
-//       { new: true, overwrite: true }
-//     );
-//     response.status(200).send(updatedUser);
-//   } catch (err) {
-//     response.status(500).send("Error updating the books information");
-//   }
-// })
-
 app.post('/profile', (request, response) => {
   try {
     let { email, homeLat, homeLon, curEmployer, curSalary, curRemote, commuteDist, milesPerGal } = request.body;
@@ -126,87 +88,29 @@ app.post('/profile', (request, response) => {
     response.status(500).send('Error in server');
   }
 });
+app.put('/newoffer/:id', async(request, response) => {
+  try {
+    console.log('this is a new offer', request.body);
+    const id = request.params.id;
+    console.log('id', id);
 
+    let updateUser = await UserModel.findByIdAndUpdate(id, request.body,{ new: true, overwrite: true });
+    console.log('before retrievedUser ',retrievedUser.newJob);
+    retrievedUser.newJob = request.body
+    console.log('after retrievedUser ',retrievedUser.newJob);
 
-
-class Offer {
-  constructor(data) {
-    this.newSalary = data.newSalary;
-    this.newEmployer = data.newEmployer;
-    this.newRemote = data.newRemote;
-    this.newLocation = data.newLocation;
+    response.status(200).send(updateUser);
+  } catch (err) {
+    response.status(500).send('Error in server when adding offer');
   }
-}
-
-// app.post('/profile', (request, response) => {
-//   try {
-//     let { email, homeLat, homeLon, curEmployer, curSalary, curRemote, commuteDist, milesPerGal } = request.body;
-//     let newUser = new UserModel({ email, homeLat, homeLon, curEmployer, curSalary, curRemote, commuteDist, milesPerGal });
-//     newUser.save();
-//     console.log(newUser);
-//     response.status(200).send('user added!');
-//   } catch (err) {
-//     response.status(500).send('Error in server');
-//   }
-// });
-
-// app.post('./newoffer', (request, response) => {
-//   try {
-//     let {newSalary, newEmployer, newRemote, newLocation, workLat, workLon, } = request.body;
-//   new UserModel({
-//     newJob: [
-//       newSalary,
-//       newEmployer,
-//       newRemote,
-//       newLocation,
-//       workLat,
-//       workLon,
-//     ]
-//   })
-// } catch (error) {
-//   console.log(error)
-// }
-
-// })
-
-
-
-
-
-
-
-
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/remote-rate', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(async () => {
-  console.log('Connected to the database');
 });
 
-require('dotenv').config();
-
-const UserModel = require('./models/users.js');
-const PORT = process.env.PORT;
-
-// const fuel = require('./modules/fuel.js');
-
-// app.get('/fuel', fuelHandler);
 
 app.get('/seed', seed);
 app.get('/clear', clear);
 
-// function fuelHandler(request, response) {
-//   const { lat, lon } = request.query;
-//   fuel(lat, lon)
-//     .then(avgFuel => response.send(avgFuel))
-//     .catch((error) => {
-//       console.error(error);
-//       response.status(200).send('Sorry. Something went wrong!');
-//     });
-// }
 
+// Functions
 function seed(request, response) {
   let users = UserModel.find({});
   if (users.length < 3) {
@@ -248,7 +152,6 @@ async function addUser(obj) {
   let newUser = new UserModel(obj);
   return await newUser.save();
 }
-
 async function clear(request, response) {
   try {
     await UserModel.deleteMany({});
